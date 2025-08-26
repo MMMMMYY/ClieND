a3dafbeca05906ac972b8682f8b0f2c5290447db
MMMMMYY <90254264+MMMMMYY@users.noreply.github.com>
2025-08-26 16:51:03 +1000

    Update training.py

--- training.py
+++ training.py
@@ -2,12 +2,12 @@ import torch
 from attacks import *
 
 def train(model, train_loader, criterion, optimizer, epochs=1, device='cpu'):
-    model.to(device)  # 确保模型在指定设备上
+    model.to(device) 
     model.train()
     for epoch in range(epochs):
         for data, target in train_loader:
             # print(f"Data batch size: {data.size(0)}, Target batch size: {target.size(0)}")
-            data, target = data.to(device), target.to(device)  # 将数据和标签移到指定设备上
+            data, target = data.to(device), target.to(device)  
             optimizer.zero_grad()
             output = model(data)
             # print(output)
@@ -18,11 +18,11 @@ def train(model, train_loader, criterion, optimizer, epochs=1, device='cpu'):
     return model
 
 def train_dropout(model, train_loader, criterion, optimizer, epochs=1, device='cpu'):
-    model.to(device)  # 确保模型在指定设备上
+    model.to(device)  
     model.train()
     for epoch in range(epochs):
         for data, target in train_loader:
-            data, target = data.to(device), target.to(device)  # 将数据和标签移到指定设备上
+            data, target = data.to(device), target.to(device) 
             optimizer.zero_grad()
             output = model(data, attack_enabled=True)
             loss = criterion(output, target)
@@ -32,21 +32,19 @@ def train_dropout(model, train_loader, criterion, optimizer, epochs=1, device='c
 
 
 def train_malicious(model, train_loader, criterion, optimizer, target_class, epochs=1, device='cpu', flip_labels=2, specific_labels=None):
-    model.to(device)  # 确保模型在指定设备上
+    model.to(device)  
     model.train()
     for epoch in range(epochs):
         for data, target in train_loader:
-            data, target = data.to(device), target.to(device)  # 将数据和标签移到指定设备上
-
-            # 如果指定了特定的标签进行翻转
+            data, target = data.to(device), target.to(device) 
             if specific_labels:
                 indices = [i for i, t in enumerate(target) if t.item() in specific_labels]
-                flip_indices = indices[:flip_labels]  # 选择前 flip_labels 个索引进行翻转
+                flip_indices = indices[:flip_labels]  
             else:
-                # 随机选择 flip_labels 个索引来翻转标签
+                
                 flip_indices = torch.randperm(target.size(0))[:flip_labels]
 
-            target[flip_indices] = target_class  # 只翻转这些标签
+            target[flip_indices] = target_class  
 
             output = model(data)
             loss = criterion(output, target)
@@ -74,13 +72,13 @@ def train_disributed_attack(model, train_loader, criterion, optimizer, epochs=1,
         return model
 
 def test(model, test_loader, device='cpu'):
-    model.to(device)  # 确保模型在指定设备上
+    model.to(device)  
     model.eval()
     correct = 0
     total = 0
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)  # 将数据和标签移到指定设备上
+            data, target = data.to(device), target.to(device)  
             output = model(data)
             _, predicted = torch.max(output.data, 1)
             total += target.size(0)
